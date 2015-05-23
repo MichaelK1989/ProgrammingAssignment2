f332c1fd5ec3fba5c5478369a904a5a07241004e
MichaelK1989 <michael.kocak89@outlook.com>
2015-05-23 13:05:41 +0200

    R-ProgrammingAssignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
-## Put comments here that give an overall description of what your
-## functions do
+## this function can be used to create a matrix and calculate and store the inverse of said matrix
+## maximum of one matrix can be stored in the cache at any moment in time
 
-## Write a short comment describing this function
-
+## makeCacheMatrix creates a special "matrix" object that can cache its inverse
 makeCacheMatrix <- function(x = matrix()) {
+	m <- NULL
+	set <- function(y){
+	x <<- y
 
+## store the matrix in the cache
+	m <<- NULL
+	}
+
+## get the matrix
+	get <- function() x
+
+## set the inverse of said matrix
+	setinverse <- function(solve) m<<- solve
+
+## get the inverse of said matrix	
+	getinverse <- function() m
+
+## create an ordered collection of objects to catch all neccesary functions from makeCacheMatrix in
+	list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
+## this function returns a matrix that is the inverse of x
+## first it checks if the solution is available in the cache and returns it if TRUE
+## if FALSE it caculates the inverse and stores it in the cache
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+## try to get the matrix from the cache
+	m <- x$getinverse()
+
+## if the cache is NOT empty return a message followed by said matrix
+	if(!is.null(m)){
+		message("getting your answer from the cache, time is money my friend")
+		return(m)
+	}
+
+## if the cache is empty, get the matrix from makeCacheMatrix and calculate the inverse
+## return a message that calculation is being done
+	message("inverse not in the cache, generating the inverse and storing it in the cache now")
+	data <- x$get()
+	m <- solve(data, ...)
+
+## store the inverse of said matrix in the cache
+	x$setinverse(m)
+
+##return the inversed matrix, not part of the assignment but good for checking expected result
+	m
 }
